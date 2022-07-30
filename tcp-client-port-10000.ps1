aeaf1ea26e22c9f0bc94d64ae2663bbd666c46b6
Eliezer Croitoru <ngtech1ltd@gmail.com>
2022-07-30 01:21:01 -0700

    1

---
+++ tcp-client-port-10000.ps1
@@ -0,0 +1,7 @@
+﻿$Port = 10000
+$IP = "127.0.0.1"
+$Address = [System.Net.IPAddress]::Parse($IP) 
+$Socket = New-Object System.Net.Sockets.TCPClient($Address,$Port)
+$Stream = $Socket.GetStream()
+$Stream.Close()
+$Socket.Close()
---
+++ tcp-servert-port-10000.ps1
@@ -0,0 +1,8 @@
+﻿$port = 10000
+$endpoint = new-object System.Net.IPEndPoint([ipaddress]::any,$port) 
+$listener = new-object System.Net.Sockets.TcpListener $EndPoint
+$listener.start()
+$data = $listener.AcceptTcpClient() 
+$stream = $data.GetStream() 
+$stream.close()
+$listener.stop()
